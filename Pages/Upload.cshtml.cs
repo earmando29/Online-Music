edc470024314237f43dc88633cd87a64cbdd6528
earmando29 <earmando29@gmail.com>
2023-03-25 17:28:48 -0500

    deleted some things

--- Pages/Upload.cshtml.cs
+++ Pages/Upload.cshtml.cs
@@ -17,49 +17,37 @@ namespace MyWebApp.Pages
             _config = config;
             _logger = logger;
         }
-
-        [BindProperty]
-        public string Title { get; set; }
         
-        [BindProperty]
-        public string Artist { get; set; }
-        
-        [BindProperty]
-        public string Genre { get; set; }
-        
-        [BindProperty]
-        public IFormFile File { get; set; }
-
         public void OnGet()
         {
         }
 
-        public IActionResult OnPost()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+        // public IActionResult OnPost()
+        // {
+        //     if (!ModelState.IsValid)
+        //     {
+        //         return Page();
+        //     }
 
-            // Get the connection string from appsettings.json
-            string connectionString = _config.GetConnectionString("MySqlConnection");
+        //     // Get the connection string from appsettings.json
+        //     string connectionString = _config.GetConnectionString("MySqlConnection");
 
-            // Connect to the database
-            using MySqlConnection connection = new MySqlConnection(connectionString);
-            connection.Open();
+        //     // Connect to the database
+        //     using MySqlConnection connection = new MySqlConnection(connectionString);
+        //     connection.Open();
 
-            // Insert the data into the database
-            string sql = "INSERT INTO songs (title, artist, genre, filename) VALUES (@title, @artist, @genre, @filename)";
-            using MySqlCommand command = new MySqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@title", Title);
-            command.Parameters.AddWithValue("@artist", Artist);
-            command.Parameters.AddWithValue("@genre", Genre);
-            command.Parameters.AddWithValue("@filename", File.FileName);
-            command.ExecuteNonQuery();
+        //     // Insert the data into the database
+        //     string sql = "INSERT INTO songs (title, artist, genre, filename) VALUES (@title, @artist, @genre, @filename)";
+        //     using MySqlCommand command = new MySqlCommand(sql, connection);
+        //     command.Parameters.AddWithValue("@title", Title);
+        //     command.Parameters.AddWithValue("@artist", Artist);
+        //     command.Parameters.AddWithValue("@genre", Genre);
+        //     command.Parameters.AddWithValue("@filename", File.FileName);
+        //     command.ExecuteNonQuery();
 
-            _logger.LogInformation("Data inserted into database");
-            connection.Close();
-            return RedirectToPage("Index");
-        }
+        //     _logger.LogInformation("Data inserted into database");
+        //     connection.Close();
+        //     return RedirectToPage("Index");
+        // }
     }
 }
